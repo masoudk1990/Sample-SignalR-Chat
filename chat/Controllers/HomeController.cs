cbe257426880bfd2d7398c15c3136b5f652deb4e
Masoud <masoudk1990@yahoo.com>
2021-03-27 20:13:47 +0430

    Initialize

---
+++ chat/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace chat.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Chat()
+        {
+
+            if (Session["user"] == null) return RedirectToAction("Login", "User");
+            return View();
+
+        }
+    }
+}
---
+++ chat/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace chat.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Submit_Login(string name)
+        {
+            Session["user"] = name;
+            return RedirectToAction("Chat", "Home");
+        }
+
+    }
+
+}
